ac2c8e6cf9d485ecc703793ae68ed161023baaa7
liaz.kamper <liaz.kamper@appsflyer.com>
2020-08-02 11:32:28 +0300

    AppsFlyer SDK 6 demo initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+![image](https://user-images.githubusercontent.com/61788924/89123513-9f5e4000-d4d8-11ea-9025-653c9f290ed8.png)
+
+# **Welcome to AppsFlyer SDK 6.0 (currently beta)**
+
+Using AppsFlyer SDK 6.0 beta is a key way for you to prepare for iOS 14. The SDK beta is compatible with the beta version of the iOS 14 and supports AppsFlyer's aggregate attribution (link to blog) and Apple's AppTrackingTransperancy framework, among other functionality. We encourage you to get ready for iOS 14 by testing and ensuring that your app is running smoothly ahead of the expected changes.
+
+## Compatability
+- **iOS version >= 10**
+
+- **AppsFlyer SDK version 6.0.0 (currently beta)**
+
+## How do I test IDFA collection?
+
+When the application initialises in iOS 14:
+
+![idfa_menu](https://user-images.githubusercontent.com/61788924/89154579-cd429380-d56f-11ea-96be-0a39e2bf1350.png)
+
+This code shows a menu that requests the user to authorise IDFA collection.
+
+```swift
+if #available(iOS 14, *) {
+            AppsFlyerLib.shared().waitForAdvertisingIdentifier(withTimeoutInterval: 60)
+            ATTrackingManager.requestTrackingAuthorization { (status) in
+            }
+        }
+```
+
+In the app you have a button '**Test IDFA collection**'
+
+#### If user authorised IDFA collection, its IDFA will be displayed
+
+![idfa_ok](https://user-images.githubusercontent.com/61788924/89153007-bfd7da00-d56c-11ea-8b04-1763bd184a5f.png)
+
+#### If user DID NOT authorise IDFA collection, a message will be displayed
+
+![idfa_not_allowed](https://user-images.githubusercontent.com/61788924/89152971-acc50a00-d56c-11ea-9198-6b3af746be10.png)
+
+## How to use this demo?
+
+1. Clone the repository
+2. **Make sure to open the work environment via Xcode workspace `basic_app.xcworkspace`**
+3. Get your AppsFlyer Dev Key using [these instructions][get_af_devkey].
+4. Create the file `sdk-v6-demo/afdevkey_donotpush.plist` with the following content:
+```xml
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+        <key>appsFlyerDevKey</key>
+        <string>YOUR_AF_DEV_KEY_HERE</string>
+        <key>appleAppID</key>
+        <string>YOUR_APPLE_APP_ID_HERE</string>
+</dict>
+</plist>
+```
+5. Add the file into your Xcode project
+> The file `sdk-v6-demo/sdk-v6-demo.xcodeproj/project.pbxproj` will have some changes. **Do not commit them!**
+
+[get_af_devkey]: https://support.appsflyer.com/hc/en-us/articles/207032066-iOS-SDK-integration-for-developers#integration-31-retrieving-your-dev-key
---
+++ swift/sdk-v6-demo/sdk-v6-demo/AppDelegate.swift
@@ -0,0 +1,129 @@
+//
+//  AppDelegate.swift
+//  basic_app
+//
+//  Created by Liaz Kamper on 11/05/2020.
+//  Copyright © 2020 OneLink. All rights reserved.
+//
+
+import UIKit
+import AppTrackingTransparency
+import AppsFlyerLib
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // 1 - Get AppsFlyer preferences from .plist file
+        guard let propertiesPath = Bundle.main.path(forResource: "afdevkey_donotpush", ofType: "plist"),
+            let properties = NSDictionary(contentsOfFile: propertiesPath) as? [String:String] else {
+                fatalError("Cannot find `afdevkey_donotpush`")
+        }
+        guard let appsFlyerDevKey = properties["appsFlyerDevKey"],
+                   let appleAppID = properties["appleAppID"] else {
+            fatalError("Cannot find `appsFlyerDevKey` or `appleAppID` key")
+        }
+        // 2 - Replace 'appsFlyerDevKey', `appleAppID` with your DevKey, Apple App ID
+        AppsFlyerLib.shared().appsFlyerDevKey = appsFlyerDevKey
+        AppsFlyerLib.shared().appleAppID = appleAppID        
+        AppsFlyerLib.shared().delegate = self
+        //  Set isDebug to true to see AppsFlyer debug logs
+        AppsFlyerLib.shared().isDebug = true      
+        
+        // The following block is for applications wishing to collect IDFA.
+        // for iOS 14 and above - The user will be prompted for permission to collect IDFA.
+        //                        If permission granted, the IDFA will be collected by the SDK.
+        // for iOS 13 and below - The IDFA will be collected by the SDK. The user will NOT be prompted for permission.
+        if #available(iOS 14, *) {
+            AppsFlyerLib.shared().waitForAdvertisingIdentifier(withTimeoutInterval: 60)
+            ATTrackingManager.requestTrackingAuthorization { (status) in
+            }
+        }
+        
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Start the SDK (start the IDFA timeout set above, for iOS 14 or later)
+        AppsFlyerLib.shared().start()
+    }
+
+    
+    // Open Univerasal Links
+    
+    // For Swift version < 4.2 replace function signature with the commented out code
+    // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool { // this line for Swift < 4.2
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        AppsFlyerLib.shared().continue(userActivity, restorationHandler: nil)
+        return true
+    }
+    
+    // Open Deeplinks
+    
+    // Open URI-scheme for iOS 8 and below
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, sourceApplication: sourceApplication, withAnnotation: annotation)
+        return true
+    }
+    
+    // Open URI-scheme for iOS 9 and above
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        AppsFlyerLib.shared().handleOpen(url, options: options)
+        return true
+    }
+    
+    // Report Push Notification attribution data for re-engagements
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        AppsFlyerLib.shared().handlePushNotification(userInfo)
+    }
+}
+
+extension AppDelegate: AppsFlyerLibDelegate {
+     
+    // Handle Organic/Non-organic installation
+    func onConversionDataSuccess(_ data: [AnyHashable: Any]) {
+        
+        print("onConversionDataSuccess data:")
+        for (key, value) in data {
+            print(key, ":", value)
+        }
+        
+        if let status = data["af_status"] as? String {
+            if (status == "Non-organic") {
+                if let sourceID = data["media_source"],
+                    let campaign = data["campaign"] {
+                    print("This is a Non-Organic install. Media source: \(sourceID)  Campaign: \(campaign)")
+                }
+            } else {
+                print("This is an organic install.")
+            }
+            if let is_first_launch = data["is_first_launch"] as? Bool,
+                is_first_launch {
+                print("First Launch")
+                
+            } else {
+                print("Not First Launch")
+            }
+        }
+    }
+    
+    func onConversionDataFail(_ error: Error) {
+        print("\(error)")
+    }
+     
+    // Handle Deeplink
+    func onAppOpenAttribution(_ attributionData: [AnyHashable: Any]) {
+        //Handle Deep Link Data
+        print("onAppOpenAttribution data:")
+        for (key, value) in attributionData {
+            print(key, ":",value)
+        }
+    }
+    
+    func onAppOpenAttributionFailure(_ error: Error) {
+        print("\(error)")
+    }
+}
